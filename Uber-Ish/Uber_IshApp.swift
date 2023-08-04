997fa265a5ca46f9ffe6d2dc3b7d056abb87d4a4
Derrick White <gr8white.contact@gmail.com>
2023-08-04 14:49:36 -0500

    Initial Commit

---
+++ Uber-Ish/Uber_IshApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uber_IshApp.swift
+//  Uber-Ish
+//
+//  Created by Cool-Ish on 8/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Uber_IshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
