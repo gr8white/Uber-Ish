391f5ce8090470002b4eaf9d5b1fc86ac44e630c
Derrick White <gr8white.contact@gmail.com>
2023-08-17 22:23:00 -0500

    Side Menu Presentation Logic

--- Uber-Ish/Core/Home/Views/HomeView.swift
+++ Uber-Ish/Core/Home/Views/HomeView.swift
@@ -9,43 +9,21 @@ import SwiftUI
 
 struct HomeView: View {
     @State private var mapViewState: MapViewState = .noInput
+    @State private var showSideMenu: Bool = false
     @EnvironmentObject var viewModel: LocationSearchViewModel
     @EnvironmentObject var authViewModel: AuthenticationViewModel
     
     var body: some View {
         Group {
             if authViewModel.userSession != nil {
-                ZStack(alignment: .bottom) {
-                    ZStack(alignment: .top) {
-                        UberMapViewRepresentable(mapViewState: $mapViewState)
-                            .ignoresSafeArea()
-                        
-                        if mapViewState == .searchingForLocation {
-                            LocationSearchView(mapViewState: $mapViewState)
-                        } else if mapViewState == .noInput {
-                            LocationSearchActivationView()
-                                .padding(.top, 72)
-                                .padding(.horizontal, 32)
-                                .onTapGesture {
-                                    withAnimation(.spring()) {
-                                        mapViewState = .searchingForLocation
-                                    }
-                                }
-                        }
-                        
-                        MapViewActionButton(mapViewState: $mapViewState)
-                            .padding(.leading)
-                            .padding(.top, 4)
+                ZStack {
+                    if showSideMenu {
+                        SideMenuView()
                     }
                     
-                    if mapViewState == .locationSelected || mapViewState == .polylineAdded {
-                        RideRequestView()
-                            .transition(.move(edge: .bottom))
-                    }
-                }
-                .edgesIgnoringSafeArea(.bottom)
-                .onReceive(LocationManager.shared.$userLocation) { location in
-                    viewModel.userLocation = location
+                    mapView
+                        .offset(x: showSideMenu ? 316: 0)
+                        .shadow(color: showSideMenu ? .black : .clear, radius: 10)
                 }
             } else {
                 LandingView()
@@ -54,8 +32,47 @@ struct HomeView: View {
     }
 }
 
+extension HomeView {
+    var mapView: some View {
+        ZStack(alignment: .bottom) {
+            ZStack(alignment: .top) {
+                UberMapViewRepresentable(mapViewState: $mapViewState)
+                    .ignoresSafeArea()
+                
+                if mapViewState == .searchingForLocation {
+                    LocationSearchView(mapViewState: $mapViewState)
+                } else if mapViewState == .noInput {
+                    LocationSearchActivationView()
+                        .padding(.top, 72)
+                        .padding(.horizontal, 32)
+                        .onTapGesture {
+                            withAnimation(.spring()) {
+                                mapViewState = .searchingForLocation
+                            }
+                        }
+                }
+                
+                MapViewActionButton(mapViewState: $mapViewState, showSideMenu: $showSideMenu)
+                    .padding(.leading)
+                    .padding(.top, 4)
+            }
+            
+            if mapViewState == .locationSelected || mapViewState == .polylineAdded {
+                RideRequestView()
+                    .transition(.move(edge: .bottom))
+            }
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .onReceive(LocationManager.shared.$userLocation) { location in
+            viewModel.userLocation = location
+        }
+    }
+}
+
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .environmentObject(LocationSearchViewModel())
+            .environmentObject(AuthenticationViewModel())
     }
 }
