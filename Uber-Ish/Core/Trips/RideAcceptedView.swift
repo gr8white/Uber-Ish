f5089e4a522052a7ff3d5bcb21609a745ca957d6
Derrick White <gr8white.contact@gmail.com>
2023-08-28 22:38:35 -0500

    RideLoadingView & RideAcceptedView

--- Uber-Ish/Core/Trips/RideAcceptedView.swift
+++ Uber-Ish/Core/Trips/RideAcceptedView.swift
@@ -10,8 +10,94 @@ import SwiftUI
 struct RideAcceptedView: View {
     var body: some View {
         VStack {
-            Text("Your driver is on the way")
+            Capsule()
+                .foregroundColor(Color(.systemGray5))
+                .frame(width: 48, height: 6)
+                .padding(.top, 12)
+            
+            VStack {
+                HStack {
+                    Text("Meet your driver at Apple Campus for your trip to Starbucks")
+                        .font(.body)
+                        .frame(height: 44)
+                        .lineLimit(2)
+                        .padding(.trailing)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Text("10")
+                        
+                        Text("min")
+                    }
+                    .frame(width: 56, height: 56)
+                    .foregroundColor(.white)
+                    .bold()
+                    .background(Color(.systemBlue))
+                    .cornerRadius(10)
+                }
                 .padding()
+            }
+            
+            Divider()
+            
+            VStack {
+                HStack(alignment: .center) {
+                    Image("male-profile-photo")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 80, height: 80)
+                        .clipShape(Circle())
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Test User")
+                            .fontWeight(.bold)
+                        
+                        HStack {
+                            Image(systemName: "star.fill")
+                                .foregroundColor(Color(.systemYellow))
+                                .imageScale(.small)
+                            
+                            Text("4.8")
+                                .font(.footnote)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                    
+                    Spacer()
+                    
+                    VStack(alignment: .center) {
+                        Image("uber-x")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 120, height: 64)
+                        
+                        HStack {
+                            Text("Mercedes S -")
+                                .foregroundColor(.gray)
+                            
+                            Text("PKC4518")
+                        }
+                        .font(.system(size: 14, weight: .semibold))
+                        .frame(width: 160)
+                    }
+                }
+                
+                Divider()
+            }
+            .padding()
+            
+            Button {
+                print("cancel ride tapped")
+            } label: {
+                Text("CANCEL RIDE")
+                    .fontWeight(.bold)
+                    .frame(width: UIScreen.main.bounds.width - 32, height: 50)
+                    .background(Color(.red))
+                    .cornerRadius(10)
+                    .foregroundColor(.white)
+            }
+            .padding(.bottom, 36)
         }
         .background(Color.theme.backgroundcolor)
         .cornerRadius(32)
--- Uber-Ish/Core/Trips/RideLoadingView.swift
+++ Uber-Ish/Core/Trips/RideLoadingView.swift
@@ -10,8 +10,28 @@ import SwiftUI
 struct RideLoadingView: View {
     var body: some View {
         VStack {
-            Text("Finding you a ride")
-                .padding()
+            Capsule()
+                .foregroundColor(Color(.systemGray5))
+                .frame(width: 48, height: 6)
+                .padding(.top, 12)
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Finding you a ride")
+                        .font(.headline)
+                    
+                    Text("Arriving at 1:30 PM")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color(.systemBlue))
+                }
+                
+                Spacer()
+                
+                LoadingSpinner(lineWidth: 6, height: 64, width: 64)
+            }
+            .padding()
+            .padding(.bottom, 24)
         }
         .background(Color.theme.backgroundcolor)
         .cornerRadius(32)
